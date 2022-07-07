993c042afe71e4ae167fef2688c6f5677a8c6a16
Zayd Alashini <86687700+7ayd@users.noreply.github.com>
2022-07-07 01:12:47 -0700

    generated full quotes and can now generate more quotes new quotes with a button

--- index.js
+++ index.js
@@ -1,25 +1,51 @@
 function App() {
-    
-    const [quotes, setQuotes] = React.useState([]); 
+
+    const [quotes, setQuotes] = React.useState([]);
     const [ranQuotes, setRanQuotes] = React.useState([]);
 
-    React.useEffect(()=> {
-        async function FetchData(){
+    React.useEffect(() => {
+        async function FetchData() {
             const response = await fetch("http://type.fit/api/quotes")
             const data = await response.json()
-            
+
             setQuotes(data);
             let ranIndex = Math.floor(Math.random() * data.length)
-            setRanQuotes(data[ranIndex]); 
+            setRanQuotes(data[ranIndex]);
         }
         FetchData();
     })
-    
-    return( 
-    <div>Hello World 
-        {quotes.map(quote => 
-            <div>{quote.text}</div>)}
-    </div>
+
+    const getNewQuote = () => {
+        let ranIndex = Math.floor(Math.random() * quotes.length)
+        setRanQuotes(quotes[ranIndex])
+    }
+
+    return (
+        <div className="container pt-5" >
+            <div className="jumbotron">
+                <div className="card">
+                    <div className="card-header"> Happy quotes </div>
+                    <div className="card-body">  {(ranQuotes) ? (
+                        <>
+                            <h5 className="card-title"> - {ranQuotes.author || "No Author"} </h5>
+                            <p className="card-text"> {ranQuotes.text}</p>
+                        </>
+                    ) : (
+                        <h2>Loading...</h2>
+                    )
+                    }
+                        <div className="row">
+                            <button onClick={getNewQuote} className="btn btn-primary ml-3" > New Quote</button>
+                            <a href="" className="btn btn-warning">
+                                <i className="fa fa-twiiter"></i>
+                            </a>
+                            <a href="" className="btn btn-danger"></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div >
     )
 }
-ReactDOM.render(<App/>, document.getElementById('app'))
+ReactDOM.render(<App />, document.getElementById('app'))
